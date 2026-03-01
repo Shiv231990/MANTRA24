a374482f229ce672ec449c9ecf72963efd3aa38a
shivam23m <shivam23may@gmail.com>
2026-03-01 18:22:30 +0530

    Final structure fix: Moved build.sh to root and cleaned core folder

--- core/apps/cart/cart/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .cart import Cart
-
-def cart(request):
-    cart_obj = Cart(request)
-    return {
-        'cart': cart_obj,
-        'cart_count': sum(item['quantity'] for item in cart_obj)
-    }
