2f1f1db1c8636fc2c6faa28fc84394d83e347c63
Shiv231990 <161128636+Shiv231990@users.noreply.github.com>
2026-03-30 19:47:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Convert static files and sync database
 python manage.py collectstatic --no-input
 python manage.py migrate
